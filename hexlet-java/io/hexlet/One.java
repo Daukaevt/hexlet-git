40b8b081a3d1d80223f5bfa2dce07aa6dab348a7
Daukaevt <mupbam1@gmail.com>
2022-09-10 09:14:34 +0300

    reverse line added

--- hexlet-java/io/hexlet/One.java
+++ hexlet-java/io/hexlet/One.java
@@ -1,7 +1,14 @@
 package io.hexlet;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class One {
     public static void main(String[] args) {
-        Two.greeting();
+    		//Two two = new Two();
+	   	//two.greeting();
+		String str = "Hexlet";
+		System.out.println(StringUtils.reverse(str));
     }
+
 }
+
